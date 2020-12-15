405e249d9332d239ec343fdb64aaf35d972fcbe8
shivaJava <shivacode.mail@gmail.com>
2020-12-15 08:05:59 +0300

    add solution for 01-basics 01-counterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,14 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
+const app = new Vue({
+  el: '#app',
+  data: () => ({
+    counter: 0
+  }),
+  methods: {
+    countHandle() {
+      ++this.counter
+    }
+  }
+})
 // Рекомендуется использовать МЕТОД в качестве обработчика события
