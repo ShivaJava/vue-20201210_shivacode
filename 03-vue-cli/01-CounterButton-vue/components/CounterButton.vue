a026649d870f476720aabc4cafa4bd046ccaddf7
shivaJava <shivacode.mail@gmail.com>
2021-01-16 11:39:23 +0300

    @Vue/cli SFC: CounterButton.vue FormGroup AppToast

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,26 @@
-<template><div></div></template>
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+  methods: {
+    increment() {
+      // При клике на кнопку порождаем событие и отправляем новое значение
+      this.$emit('increment', this.count + 1);
+    },
+  }
+};
 </script>
 
 <style></style>
